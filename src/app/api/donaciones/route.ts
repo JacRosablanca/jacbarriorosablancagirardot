2b7b44c177a352c3fdde03053f5f1cceb4684d26
JacRosablanca <juntacomunalrosablanca@gmail.com>
2025-12-02 16:33:36 -0500

    Actualizar route.ts

--- src/app/api/donaciones/route.ts
+++ src/app/api/donaciones/route.ts
@@ -1,17 +1,15 @@
-// /app/api/donaciones/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { google } from 'googleapis';
 import { SPREADSHEET_ID_DONACIONES, SHEET_NAME_DONACIONES, GOOGLE_CLIENT_EMAIL, GOOGLE_PRIVATE_KEY } from '../../../config/idSheets';
 
 export async function POST(req: NextRequest) {
   try {
-    const { phoneNumber, password, amount, selectedActivity, paymentMethod } = await req.json();
+    const { phoneNumber, password, amount } = await req.json();
 
-    if (!phoneNumber || !password || !amount || !selectedActivity || !paymentMethod) {
+    if (!phoneNumber || !password || !amount) {
       return NextResponse.json({ error: 'Faltan datos' }, { status: 400 });
     }
 
-    // Configurar cliente de Google Sheets
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: GOOGLE_CLIENT_EMAIL,
@@ -22,7 +20,6 @@ export async function POST(req: NextRequest) {
 
     const sheets = google.sheets({ version: 'v4', auth });
 
-    // Insertar fila nueva
     await sheets.spreadsheets.values.append({
       spreadsheetId: SPREADSHEET_ID_DONACIONES,
       range: `${SHEET_NAME_DONACIONES}!A:C`,
