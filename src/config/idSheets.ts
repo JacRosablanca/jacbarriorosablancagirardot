4e080fd2408d3fdf4a4822e3c8075ea17e5f08be
JacRosablanca <juntacomunalrosablanca@gmail.com>
2025-12-02 23:00:37 -0500

    Actualizar idSheets.ts

--- src/config/idSheets.ts
+++ src/config/idSheets.ts
@@ -1,76 +1,87 @@
-// src/config/idSheets.ts
+// /config/idSheets.tsx
 
-//credenciales
+// Credenciales Google Service Account
 export const GOOGLE_CLIENT_EMAIL = "jacrosablanca1girardot@teak-brook-456920-t2.iam.gserviceaccount.com";
+export const GOOGLE_PRIVATE_KEY = `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZK9qDkg9l1Fno
+E4UUuD6w/6S73hywafD+DhCcjxWv1lOY7ZMKdKnvdlB0E4oyWIu6xIlyTekPcDaW
+GHhSAbhWSpSBuflRuA3BYcTwSU8qCU27qoRklC9FWlpEQR1T/JTgp1TIRmLL3aym
+WfoLDcWNnmFajjXfh0I3OopycnG71pFgPAK+yl2KITcW24JMQFWohXEa23QV8o4i
+M/pt97734UuXeKPqKGTh3Nt5/0RAw1ZaFIVnOaKKiZk3DDVPL5zd/zSgVOVeEtnt
+hyOwiJijjc1fajBwBhmO1YDmcmu4IVrPnkGVU534ZVc6j6rCQyrHyeeLaomVgO0u
+06FraUv9AgMBAAECggEAAi9s+6mOzK4d5kC5ClWil1XeXMvLbvzj8kvzEri733lY
+Ufs5uNF3RzJR7YYF17gJrk11hIr2A0Fflf6dYNYjch+4RK2JkA/nKlYhwCfbmEbv
+s/i30NOIZR3n4cdoCrW0zgu1GvJeIHh2KR1pgtTcd6vFycvzms/MMwuDxCQ72Skt
+3UOtL4VF9mbwNTM4xIQ+hhEcqHT1gFFq+zZS+5WBwUQiUKQNA6suD4M29In9FuzT
+w7YTMzDAMVfyen4sAL2BUgqOyrDjCWHlE/IqGxLS8PYf/YpkbklRrxmyZdTDtVRX
+IaAWBcxw5eRoc/1qCi7KZr9Tb4XZD9gLKilqi/vaQQKBgQDHnFtwiEGbL35KeQj+
+4x9tt9MiEnN9KvlC5B2dN5MnN4xiUFdnOvj95CXBEVqA1sMO22QSdxkw4NAs7aSR
+sDgggoleU6aQzqBA8/Ywrc8zK2o+Hlf1YRIneuAERirIHPHQUW6kldBwJJsePHwV
+0noh/qOfpM5zP9vZfDcluJP8bQKBgQDEcQri7do35tj4E/wjp0bz5vCJMiDJ8HjN
+H5y+tZMLoSY06EvFHKaT+lq0n7EvlmEDuDsDtC3BT+FyMJXGas5mTc3Fh6KhsoMs
+UjXCwdqFj/DrnYJ+xjECqitJ1EG93W5jWXANSU+DtVCP829Lq2HtQj4NE29MW12d
+FmNW5niz0QKBgFijV/96ohd1WZFceeAlnxkrnfftVZdpc9J+VaAww4mQoygtIQLY
+LyUfIJzBRf0cYPXB+ogwluEbk5W/2WpQZDwbe0CFyNtYxdTSAKrRI8dHKiB3fPub
+pWIesEd1mR3PrJmi74zqceq6dp9QoJFYK5/Y4XPMzVhkLQ5AmISJmD1pAoGBAK8y
+6rSt0dvdpVDL8iqSPDO3mnuiG6A/Ol4nfZKROo46sWqqazlnbiG4PUGu00ZP5kJ9
+AxUwk0Y/yiAfuNT32RFg/s7XQ3LXVSooQA5s+kn4sLRTDFmoxXlM78Ku7ds6LAOr
+dsZCxgn/odRhFrf0KCiE1faviKofKoS5vOt6LdihAoGAbaWCSja915WimeJctXr3
+OVE1MiRzpTNLrF6DoZ2/l778wZjIjI3rV9MvCKnvyu5pP5nryaaNb0MJVLMVXDRc
+O8p9ZYhlvFgZ0NGUttAe1oU4T/MMX5NPM+7O9AnGV4Rsg7xR9tpgipWwKQcyE5Lv
+gjNKSmcnj+O3AodqS+XLS80=
+-----END PRIVATE KEY-----`;
 
-export const GOOGLE_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZK9qDkg9l1Fno\nE4UUuD6w/6S73hywafD+DhCcjxWv1lOY7ZMKdKnvdlB0E4oyWIu6xIlyTekPcDaW\nGHhSAbhWSpSBuflRuA3BYcTwSU8qCU27qoRklC9FWlpEQR1T/JTgp1TIRmLL3aym\nWfoLDcWNnmFajjXfh0I3OopycnG71pFgPAK+yl2KITcW24JMQFWohXEa23QV8o4i\nM/pt97734UuXeKPqKGTh3Nt5/0RAw1ZaFIVnOaKKiZk3DDVPL5zd/zSgVOVeEtnt\nhyOwiJijjc1fajBwBhmO1YDmcmu4IVrPnkGVU534ZVc6j6rCQyrHyeeLaomVgO0u\n06FraUv9AgMBAAECggEAAi9s+6mOzK4d5kC5ClWil1XeXMvLbvzj8kvzEri733lY\nUfs5uNF3RzJR7YYF17gJrk11hIr2A0Fflf6dYNYjch+4RK2JkA/nKlYhwCfbmEbv\ns/i30NOIZR3n4cdoCrW0zgu1GvJeIHh2KR1pgtTcd6vFycvzms/MMwuDxCQ72Skt\n3UOtL4VF9mbwNTM4xIQ+hhEcqHT1gFFq+zZS+5WBwUQiUKQNA6suD4M29In9FuzT\nw7YTMzDAMVfyen4sAL2BUgqOyrDjCWHlE/IqGxLS8PYf/YpkbklRrxmyZdTDtVRX\nIaAWBcxw5eRoc/1qCi7KZr9Tb4XZD9gLKilqi/vaQQKBgQDHnFtwiEGbL35KeQj+\n4x9tt9MiEnN9KvlC5B2dN5MnN4xiUFdnOvj95CXBEVqA1sMO22QSdxkw4NAs7aSR\nsDgggoleU6aQzqBA8/Ywrc8zK2o+Hlf1YRIneuAERirIHPHQUW6kldBwJJsePHwV\n0noh/qOfpM5zP9vZfDcluJP8bQKBgQDEcQri7do35tj4E/wjp0bz5vCJMiDJ8HjN\nH5y+tZMLoSY06EvFHKaT+lq0n7EvlmEDuDsDtC3BT+FyMJXGas5mTc3Fh6KhsoMs\nUjXCwdqFj/DrnYJ+xjECqitJ1EG93W5jWXANSU+DtVCP829Lq2HtQj4NE29MW12d\nFmNW5niz0QKBgFijV/96ohd1WZFceeAlnxkrnfftVZdpc9J+VaAww4mQoygtIQLY\nLyUfIJzBRf0cYPXB+ogwluEbk5W/2WpQZDwbe0CFyNtYxdTSAKrRI8dHKiB3fPub\npWIesEd1mR3PrJmi74zqceq6dp9QoJFYK5/Y4XPMzVhkLQ5AmISJmD1pAoGBAK8y\n6rSt0dvdpVDL8iqSPDO3mnuiG6A/Ol4nfZKROo46sWqqazlnbiG4PUGu00ZP5kJ9\nAxUwk0Y/yiAfuNT32RFg/s7XQ3LXVSooQA5s+kn4sLRTDFmoxXlM78Ku7ds6LAOr\ndsZCxgn/odRhFrf0KCiE1faviKofKoS5vOt6LdihAoGAbaWCSja915WimeJctXr3\nOVE1MiRzpTNLrF6DoZ2/l778wZjIjI3rV9MvCKnvyu5pP5nryaaNb0MJVLMVXDRc\nO8p9ZYhlvFgZ0NGUttAe1oU4T/MMX5NPM+7O9AnGV4Rsg7xR9tpgipWwKQcyE5Lv\ngjNKSmcnj+O3AodqS+XLS80=\n-----END PRIVATE KEY-----\n";
-
-// infoJac
+// Hoja de cálculo: info general
 export const SPREADSHEET_ID = '1eXaCAyfkWY1ac-5KitWPKETMoSvAzuyp_NVZfYH5NDI';
-export const SHEET_NAME_HEADER = 'Header'; // Asegúrate de que este sea el nombre correcto de tu pestaña
-export const SHEET_NAME_FOOTER = 'Footer'; // Asegúrate de que este sea el nombre correcto de tu pestaña
-export const SHEET_NAME_REDES = 'Redes Sociales'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_HEADER = 'Header';
+export const SHEET_NAME_FOOTER = 'Footer';
+export const SHEET_NAME_REDES = 'Redes Sociales';
 
-//Usuarios
-export const SPREADSHEET_ID_USUARIOS = '1eXaCAyfkWY1ac-5KitWPKETMoSvAzuyp_NVZfYH5NDI';
+// Usuarios
+export const SPREADSHEET_ID_USUARIOS = SPREADSHEET_ID;
 export const SHEET_NAME_USUARIOS = "Usuarios";
 export const RANGE_USUARIOS = `${SHEET_NAME_USUARIOS}!A1:C`;
 
-// src/app/
+// Cartelera
 export const SPREADSHEET_ID_CARTELERA = '18At-UfafKNT-bPo1cK69ScHaf07E1yUKELYO6TWfSeo';
-export const SHEET_NAME_CARTELERA = 'Cartelera'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_CARTELERA = 'Cartelera';
 
-// src/app/presidencia/proyectos/
+// Proyectos
 export const SPREADSHEET_ID_PROYECTOS = '1G78mQ00liuf4s8889uoPf3_eYR3ySwTm1x8Vg8UvNPA';
-export const SHEET_NAME_PROYECTOS = 'Proyectos'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_PROYECTOS = 'Proyectos';
 
-// src/app/presidencia/citaciones/asamblea-general/
+// Asamblea general
 export const SPREADSHEET_ID_ASAMBLEA = '1CAb1WpzCGS-uL283dAkffsS2aXNYMRGu85CmQKduD7k';
-export const SHEET_NAME_ASAMBLEA = 'CitacionAsambleaGeneral'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_ASAMBLEA = 'CitacionAsambleaGeneral';
 
-// src/app/presidencia/citaciones/asamblea-general/
+// Reunion directivos
 export const SPREADSHEET_ID_REUNION_DIRECTIVOS = '1PR46lMH-1eTVhpVTL8hf0enK5eAX4i6X8_XEBN3rNUM';
-export const SHEET_NAME_REUNION_DIRECTIVOS = 'CitacionReunionDignatarios'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_REUNION_DIRECTIVOS = 'CitacionReunionDignatarios';
 
-// src/app/presidencia/citaciones/asamblea-general/
+// Asamblea residentes
 export const SPREADSHEET_ID_ASAMBLEA_RESIDENTES = '1WQCku68N6hVLzvLSv-lQzhl5LvIGDninKOwqnNnbUlc';
-export const SHEET_NAME_ASAMBLEA_RESIDENTES = 'CitacionAsambleaResidentes'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_ASAMBLEA_RESIDENTES = 'CitacionAsambleaResidentes';
 
-// src/app/presidencia/gestiones-realizadas/
+// Gestiones
 export const SPREADSHEET_ID_GESTIONES = '1EAVnNF9VTYRwcS-84vmcFmcqq-UGcQ8bquH1mnPSSnw';
-export const SHEET_NAME_GESTIONES = 'Gestiones'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_GESTIONES = 'Gestiones';
 
-// src/app/tesorería/donaciones 
+// Donaciones
 export const SPREADSHEET_ID_DONACIONES = '1nByyoOf8FArHtiS-ogPfCT3jfni8GPmysz823cDVmq8';
 export const SHEET_NAME_DONACIONES = 'Donaciones';
 
-
-
-
-
-
-
-
-
-
-
-
-// EquipoJac/
+// Equipo
 export const SPREADSHEET_ID_EQUIPO = '1NriapKLWWq40UfOmf6oz5yw1iXyij4-n7vbWAvXAnaE';
-export const SHEET_NAME_EQUIPO = 'Equipo'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_EQUIPO = 'Equipo';
 
-// ActividadesJac/
+// Actividades
 export const SPREADSHEET_ID_ACTIVIDADES = '13sVd9xwEeK357MQp61zTzSciEG3GoWFRoR18llxlYiA';
-export const SHEET_NAME_ACTIVIDADES = 'Actividades'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+export const SHEET_NAME_ACTIVIDADES = 'Actividades';
 
-// PatrocinadoresJac/
-export const SPREADSHEET_ID_PATROCINADORES = '13sVd9xwEeK357MQp61zTzSciEG3GoWFRoR18llxlYiA';
-export const SHEET_NAME_PATROCINADORES = 'Patrocinadores'; // Asegúrate de que este sea el nombre correcto de tu pestaña
+// Patrocinadores
+export const SPREADSHEET_ID_PATROCINADORES = SPREADSHEET_ID_ACTIVIDADES;
+export const SHEET_NAME_PATROCINADORES = 'Patrocinadores';
 
-// SobreNosotros/
+// Sobre Nosotros
 export const SPREADSHEET_ID_NOSOTROS = '1omG7qCrgcL7uSb7-2hbK7218f4wxQalxD8daKZRyHv0';
-export const SHEET_NAME_NOSOTROS = 'Sobre Nosotros'; // Asegúrate de que este sea el nombre correcto de tu pestaña
-
-
-
-
+export const SHEET_NAME_NOSOTROS = 'Sobre Nosotros';
