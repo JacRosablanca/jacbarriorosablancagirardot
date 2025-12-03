9e526070b09e73677bf682c15a6bdddffedb4ec8
JacRosablanca <juntacomunalrosablanca@gmail.com>
2025-12-02 22:39:25 -0500

    Crear enlocal.tsx

---
+++ src/config/enlocal.tsx
@@ -0,0 +1,6 @@
+// /config/envlocal.tsx
+
+export const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL || '';
+export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY || '';
+export const SPREADSHEET_ID_DONACIONES = process.env.SPREADSHEET_ID_DONACIONES || '';
+export const SHEET_NAME_DONACIONES = process.env.SHEET_NAME_DONACIONES || 'Donaciones';
